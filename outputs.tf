a034b963a47313f9ec1eacbcc61c45e6fa0d5921
Prasiddha Bista <prasiddhabista102@gmail.com>
2023-03-11 14:20:54 +1100

    feat(gcloud-bootstrap): testing github actions

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
-output "github_actions_identity_pool_name" {
+output github_actions_identity_pool_name {
   description = "Pool name"
   value       = google_iam_workload_identity_pool.github_actions_identity_pool.name
 }
 
-output "github_actions_identity_pool_provider_name" {
+output github_actions_identity_pool_provider_name {
   description = "Provider name"
   value       = google_iam_workload_identity_pool_provider.github_actions_identity_pool_provider.name
+}
+
+output github_actions_service_account_name {
+  description = "Github Actions service account name"
+  value       = google_service_account.github_actions_service_account.name
 }
