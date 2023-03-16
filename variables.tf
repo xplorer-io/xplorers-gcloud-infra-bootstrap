afd0d655e92f66cc7ac417180a8bbfb8764c0f07
Prasiddha Bista <prasiddhabista102@gmail.com>
2023-03-16 11:38:50 +1100

    fix(test_gcloud.yml): remove activate_credentials_file from gcloud auth action configuration
    refactor(variables.tf): change variable declarations to use quotes and align values with keys
    
    refactor(variables.tf): change variable declarations to use quotes and add descriptions to all variables

--- variables.tf
+++ variables.tf
@@ -1,75 +1,78 @@
-variable project_id {
-  type             = string
-  sensitive        = true
-  nullable         = false
+variable "project_id" {
+  type      = string
+  sensitive = true
+  nullable  = false
 }
 
-variable region {
-  type             = string
-  sensitive        = true
-  nullable         = false
+variable "region" {
+  type      = string
+  sensitive = true
+  nullable  = false
 }
 
-variable zone {
-  type             = string
-  sensitive        = true
-  nullable         = false
+variable "zone" {
+  type      = string
+  sensitive = true
+  nullable  = false
 }
 
-variable service_apis_to_enable {
-  type        = list
-  default     = [
-    "cloudresourcemanager.googleapis.com",
+variable "service_apis_to_enable" {
+  type = list(any)
+  default = [
+    "artifactregistry.googleapis.com",
+    "cloudbuild.googleapis.com",
     "cloudfunctions.googleapis.com",
-    "iam.googleapis.com"
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
+    "run.googleapis.com"
   ]
 }
 
-variable github_organisation {
-  type = string
+variable "github_organisation" {
+  type      = string
   sensitive = true
-  nullable = false
+  nullable  = false
 }
 
 ###################### GITHUB ACTIONS ######################
 
-variable pool_display_name {
+variable "pool_display_name" {
   type        = string
   description = "Workload Identity Pool display name"
   default     = "Github Actions Identity Pool"
 }
 
-variable pool_description {
+variable "pool_description" {
   type        = string
   description = "Workload Identity Pool description"
   default     = "Identity pool used by Github Actions to perform deployments"
 }
 
-variable pool_id {
+variable "pool_id" {
   type        = string
   description = "Workload Identity Pool ID"
   default     = "github-actions-identity-pool"
 }
 
-variable provider_id {
+variable "provider_id" {
   type        = string
   description = "Workload Identity Pool Provider id"
-  default = "github-actions-iam-provider"
+  default     = "github-actions-iam-provider"
 }
 
-variable provider_display_name {
+variable "provider_display_name" {
   type        = string
   description = "Workload Identity Pool Provider display name"
   default     = "Github Actions IAM Provider"
 }
 
-variable provider_description {
+variable "provider_description" {
   type        = string
   description = "Workload Identity Pool Provider description"
   default     = "Identity pool provider used by Github Actions to perform deployments"
 }
 
-variable attribute_mapping {
+variable "attribute_mapping" {
   type        = map(any)
   description = "Workload Identity Pool Provider attribute mapping. [More info](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/iam_workload_identity_pool_provider#attribute_mapping)"
   default = {
@@ -80,32 +83,32 @@ variable attribute_mapping {
   }
 }
 
-variable issuer_uri {
+variable "issuer_uri" {
   type        = string
   description = "Workload Identity Pool Issuer URL"
   default     = "https://token.actions.githubusercontent.com"
 }
 
-variable github_actions_service_account_id {
-  type = string
+variable "github_actions_service_account_id" {
+  type        = string
   description = "Github Actions service account id"
-  default = "github-actions-service-account"
+  default     = "github-actions-service-account"
 }
 
-variable github_actions_service_account_display_name {
-  type = string
+variable "github_actions_service_account_display_name" {
+  type        = string
   description = "Github Actions service account name display name"
-  default = "Service Account used for GitHub Actions"
+  default     = "Service Account used for GitHub Actions"
 }
 
-variable workload_identity_user_role {
-  type = string
+variable "workload_identity_user_role" {
+  type        = string
   description = "Workload Identity User Role"
-  default = "roles/iam.workloadIdentityUser"
+  default     = "roles/iam.workloadIdentityUser"
 }
 
-variable github_actions_cloudfunctions_role {
-  type = string
+variable "github_actions_cloudfunctions_role" {
+  type        = string
   description = "Github Actions Cloud Functions role"
-  default = "roles/cloudfunctions.developer"
+  default     = "roles/cloudfunctions.developer"
 }
